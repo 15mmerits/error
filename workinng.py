4dd991b4248e7f381ed61d202e022398101177d6
Jayprakash Kumar <80420725+15mmerits@users.noreply.github.com>
2022-04-13 03:16:53 +0530

    working
    in 7th paragraph, given url working without any eror.

---
+++ workinng.py
@@ -0,0 +1,41 @@
+import requests
+import pandas as pd 
+from bs4 import BeautifulSoup
+
+# URL of Netflix Stock site (working code)
+#url  below 
+url = 'https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-PY0220EN-SkillsNetwork/labs/project/amazon_data_webpage.html'
+data = requests.get(url).text
+soup = BeautifulSoup(data, 'html5lib')
+# print(soup.prettify)
+
+# HTML Table into Pandas DataFrame
+
+netflix_data = pd.DataFrame(columns = ['Date', 'Open', 'High', 'Low', 'Close', 'Volume'])
+print(netflix_data)
+
+# Isolating the body of table which contains all the information
+# Then we loop through each row & find all colum values for each row
+
+for row in soup.find("tbody").find_all('tr'):
+    col = row.find_all("td")
+    date = col[0].text
+    Open = col[1].text
+    high = col[2].text
+    low = col[3].text
+    close = col[4].text
+    volume = col[6].text
+    
+# Then we append the data of each row to the table
+
+netflix_data = netflix_data.append({'Date': date, 'Open': open, 'High': high, 'Low': low, 'Volume': volume}, ignore_index=True)
+
+
+# netflix_data.head() # Print DataFrame
+# # Pandas read_html
+read_html = pd.read_html(url)
+# #converting bs4 into string
+bs4_str = pd.read_html(str(soup))
+
+netflix_dataframe = bs4_str[0]
+netflix_dataframe.head(5) 
